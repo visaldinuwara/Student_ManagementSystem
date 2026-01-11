dbcfe12e90341b528e320a54b45a31152ca22035
Visal Dinuwara <visaldinuwara@gmail.com>
2026-01-11 22:14:18 +0530

    Add .gitattributes for consistent line endings

---
+++ src/main/java/com/dhampasala/student/student_management/StudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.dhampasala.student.student_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+}
